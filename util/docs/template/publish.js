100bada86c7e17c1e707e0396e11c2e0aecbf009
pamela.fox <pamela.fox@b4c4f48c-70f0-11de-9ad3-cbed566d3045>
2009-07-15 04:20:48 +0000

    Adding utility scripts, copied from v2 project

---
+++ util/docs/template/publish.js
@@ -0,0 +1,248 @@
+function publish(symbolSet) {
+  // semi-required configuration options
+  publish.conf = {
+    ext: "",
+    outDir: JSDOC.opt.d,
+    templatesDir: JSDOC.opt.t,
+    symbolsDir: "reference.html#"
+  };
+
+  // used to check the details of things being linked to
+  Link.symbolSet = symbolSet;
+
+  // Populate the library-wide properties
+  function isaFile($) {return $.is("FILE");}
+  var file = symbolSet.toArray().filter(isaFile)[0];
+  var templateValues = {
+    "library" : {
+      "name": file.comment.getTag("name"),
+      "version": file.version,
+      "desc": file.desc
+    },
+    "objects": []
+  };
+
+  // Find the top-level objects - class, enum, function, interface, namespace
+  function isaTopLevelObject($) {return (
+    ($.is("CONSTRUCTOR") && $.name.match(/^[A-Z][A-Za-z]+/)) ||
+    ($.is("FUNCTION") && $.name.match(/^[A-Z][A-Za-z]+/)) ||
+    $.comment.getTag("enum")[0] ||
+    $.comment.getTag("interface")[0] ||
+    $.isNamespace
+  );}
+
+  // Find the second-level objects - Properties, Constructor, Methods, Constants, Events
+  function isaSecondLevelObject ($) {return (
+    $.comment.getTag("property").length ||
+    ($.is("CONSTRUCTOR") && $.name.match(/^[A-Z][A-Za-z]+$/)) ||
+    ($.is("FUNCTION") && $.name.match(/^[a-z][A-Za-z]+$/)) ||
+    $.isConstant ||
+    ($.type == "Function" && $.is("OBJECT"))
+  );}
+  function hasProperties ($) {
+    return ($.comment.getTag("property").length);
+  }
+  function isaConstructor ($) {
+    return ($.is("CONSTRUCTOR") && $.name.match(/^[A-Z][A-Za-z]+$/));
+  }
+  function isaMethod ($) {
+    return (
+      $.is("FUNCTION") &&
+      $.name.match(/^[a-z][A-Za-z]+$/) &&
+      !$.isEvent &&
+      !$.isPrivate
+    );
+  }
+  function isaConstant ($) {
+    return $.isConstant;
+  }
+  function isaEvent ($) {
+    return $.isEvent;
+  }
+
+  var topLevelObjects = symbolSet.toArray().filter(function ($) {return !(
+    $.srcFile.match(/_packed/) ||
+    $.name === "_global_"
+  );}).filter(isaTopLevelObject).sort(makeSortby("alias"));
+  for (var i = 0; i < topLevelObjects.length; i++) {
+    var topLevelObject = topLevelObjects[i];
+    var constructedTopLevelObject = {
+      "name": topLevelObject.name,
+      "desc": topLevelObject.classDesc,
+      "anchor": topLevelObject.alias,
+      "properties": []
+    };
+    // Determine the type of top-level object - class, enum, function, interface, or namespace
+    if (topLevelObject.is("CONSTRUCTOR") && topLevelObject.name.match(/^[A-Z][A-Za-z]+$/)) {
+      constructedTopLevelObject.type = "class";
+      if (topLevelObject.augments.length) {
+        for (ancestor in topLevelObject.augments) {
+          constructedTopLevelObject.desc += "  This class extends {@link " + topLevelObject.augments[ancestor] + "}.";
+        }
+      }
+      if (constructedTopLevelObject.name.match(/Options$/)) {
+        constructedTopLevelObject.desc += "  There is no constructor for this class.  Instead, this class is instantiated as a javascript object literal.";
+      }
+    } else if (topLevelObject.isNamespace) {
+      constructedTopLevelObject.type = "namespace"
+      constructedTopLevelObject.name = topLevelObject.alias
+    } else {
+      constructedTopLevelObject.type = "class"; // unknown
+    }
+
+    // Find the nested Constructor
+    var constructorSymbol = symbolSet.toArray().filter(function ($) {
+      return ($.alias.replace(/^([^#]+)#.*$/, "$1") === topLevelObject.name);
+    }).filter(isaConstructor).sort(makeSortby("alias"))[0];
+    if (constructorSymbol && !constructorSymbol.name.match(/Options$/) && constructorSymbol.desc) {
+      var constructedProperty = {
+        "type": "Constructor",
+        "columns": [],
+        "instances": []
+      };
+      var instance = {
+        "Constructor": constructorSymbol.name,
+        "desc": constructorSymbol.desc
+      };
+      var constructorArguments = [];
+      for (var k = 0; k < constructorSymbol.params.length; k++) {
+        var constructorParameter = constructorSymbol.params[k];
+        var argumentString = '<span class="type">';
+        argumentString += constructorParameter.name;
+        argumentString += constructorParameter.isOptional ? "?" : "";
+        argumentString += ':' + constructorParameter.type;
+        argumentString += '</span>';
+        constructorArguments.push(argumentString);
+      }
+      instance.Constructor += "(" + constructorArguments.join(", ") + ")";
+      constructedProperty.instances.push(instance);
+      constructedTopLevelObject.properties.push(constructedProperty);
+    }
+
+    // Find the nested Properties
+    var propertySymbol = symbolSet.toArray().filter(function ($) {
+      return ($.alias.replace(/^([^#]+)#.*$/, "$1") === topLevelObject.name);
+    }).filter(hasProperties).sort(makeSortby("alias"))[0];
+    if (propertySymbol) {
+      var constructedProperty = {
+        "type": "Properties",
+        "columns": ["Type"],
+        "instances": []
+      };
+      var properties = propertySymbol.comment.getTag("property").sort(makeSortby("name"));
+      for (var j = 0; j < properties.length; j++) {
+        var thisProperty = properties[j];
+        var instance = {
+          "Type": thisProperty.type,
+          "desc": thisProperty.desc
+        };
+        if (thisProperty.defaultValue) {
+          instance.desc += "  The default value is <code>" + thisProperty.defaultValue + "</code>.";
+        }
+        instance[constructedProperty.type] = thisProperty.name;
+        constructedProperty.instances.push(instance);
+      }
+      constructedTopLevelObject.properties.push(constructedProperty);
+    }
+
+    // Find the nested Methods
+    var methodSymbols = symbolSet.toArray().filter(function ($) {
+      return ($.memberOf === topLevelObject.alias);
+    }).filter(isaMethod).sort(makeSortby("alias"));
+    if (methodSymbols.length) {
+      var otherMethodType = "";
+      if (methodSymbols[0].isStatic) {
+        otherMethodType = "Static ";
+      }
+      var constructedProperty = {
+        "type": (otherMethodType + "Methods"),
+        "columns": ["Return Value"],
+        "instances": []
+      };
+      for (var l = 0; l < methodSymbols.length; l++) {
+        var methodSymbol = methodSymbols[l];
+        var instance = {
+          "Return Value": "None",
+          "desc": methodSymbol.desc
+        };
+        if (methodSymbol.returns.length) {
+          instance["Return Value"] = methodSymbol.returns[0].type;
+        }
+        instance[otherMethodType + "Methods"] = methodSymbol.name;
+        var methodArguments = [];
+        for (var m = 0; m < methodSymbol.params.length; m++) {
+          var methodParameter = methodSymbol.params[m]
+          var argumentString = '<span class="type">';
+          argumentString += methodParameter.name;
+          argumentString += methodParameter.isOptional ? "?" : "";
+          argumentString += ":" + methodParameter.type;
+          argumentString += '</span>';
+          methodArguments.push(argumentString);
+        }
+        instance[otherMethodType + "Methods"] += "(" + methodArguments.join(", ") + ")";
+        constructedProperty.instances.push(instance);
+      }
+      constructedTopLevelObject.properties.push(constructedProperty);
+    }
+
+    // Find the nested Events
+    var eventSymbols = symbolSet.toArray().filter(function ($) {
+      return ($.memberOf === topLevelObject.alias);
+    }).filter(isaEvent).sort(makeSortby("alias"));
+    if (eventSymbols.length) {
+      var constructedProperty = {
+        "type": "Events",
+        "columns": ["Arguments"],
+        "instances": []
+      };
+      for (var l = 0; l < eventSymbols.length; l++) {
+        var thisEvent = eventSymbols[l];
+        var eventArguments = [];
+        for (var m = 0; m < thisEvent.params.length; m++) {
+          var thisArgument = thisEvent.params[m];
+          var argumentString = '<span class="type">'
+          argumentString += thisArgument.name;
+          argumentString += ':' + thisArgument.type + '</span>';
+          eventArguments.push(argumentString);
+        }
+        var instance = {
+          "Arguments": eventArguments.join(", ") || "None",
+          "desc": thisEvent.desc
+        };
+        instance[constructedProperty.type] = thisEvent.name;
+        constructedProperty.instances.push(instance);
+      }
+      constructedTopLevelObject.properties.push(constructedProperty);
+    }
+
+    templateValues.objects.push(constructedTopLevelObject);
+  }
+
+  var referenceTemplate = new JSDOC.JsPlate(publish.conf.templatesDir + "reference.tmpl");
+  var output = "";
+  output = referenceTemplate.process(templateValues);
+  IO.saveFile(publish.conf.outDir, "reference.html", output);
+}
+
+/** make a symbol sorter by some attribute */
+function makeSortby(attribute) {
+  return function(a, b) {
+    if (a[attribute] != undefined && b[attribute] != undefined) {
+      a = a[attribute].toLowerCase();
+      b = b[attribute].toLowerCase();
+      if (a < b) return -1;
+      if (a > b) return 1;
+      return 0;
+    }
+  }
+}
+
+/** Find symbol {@link ...} strings in text and turn into html links */
+function resolveLinks (str, from) {
+  str = str.replace(/\{@link ([^} ]+) ?\}/gi,
+    function(match, symbolName) {
+      return "<code>" + new Link().toClass(symbolName) + "</code>";
+    }
+  );
+  return str;
+}
---
+++ util/lint/lint.js
@@ -0,0 +1,46 @@
+// rhino.js
+// 2007-02-19
+/*
+Copyright (c) 2002 Douglas Crockford  (www.JSLint.com) Rhino Edition
+*/
+
+// This is the Rhino companion to fulljslint.js.
+
+(function (a) {
+    /*extern JSLINT */
+    importPackage(java.lang);
+
+    var pwd = "util/lint/";
+    load(pwd + "fulljslint.js");
+
+    if (!a[0]) {
+        print("Usage: jslint.js file.js");
+        quit(1);
+    }
+    filesArray = a[0].split(";");
+    for (var i = 0; i < filesArray.length; i++) {
+      var currentFile = filesArray[i];
+      print(currentFile);
+      var input = readFile(currentFile);
+      if (!input) {
+        print("jslint: Couldn't open file '" + currentFile + "'.");
+        quit(1);
+      }
+      load(pwd + "jslint.conf");
+      if (!JSLINT(input, JSLINT_OPTIONS)) {
+        for (var i = 0; i < JSLINT.errors.length; i += 1) {
+            var e = JSLINT.errors[i];
+            if (e) {
+                print('Lint at line ' + (e.line + 1) + ' character ' +
+                        (e.character + 1) + ': ' + e.reason);
+                print((e.evidence || '').
+                        replace(/^\s*(\S*(\s+\S+)*)\s*$/, "$1"));
+                print('');
+            }
+        }
+      } else {
+        print("jslint: No problems found in " + a[0]);
+        quit();
+      }
+    }
+})(arguments);
