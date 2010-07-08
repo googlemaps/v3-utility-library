742697350b97c902a8ee2cad3a1522c67b1c7c4e
nianwei <nianwei@b4c4f48c-70f0-11de-9ad3-cbed566d3045>
2010-07-08 17:24:44 +0000

    revert lint.js because new version can not invoke fulljslint.js

--- util/lint/lint.js
+++ util/lint/lint.js
@@ -1,41 +1,47 @@
 // rhino.js
-// 2009-09-11
+// 2007-02-19
 /*
 Copyright (c) 2002 Douglas Crockford  (www.JSLint.com) Rhino Edition
 */
 
 // This is the Rhino companion to fulljslint.js.
 
-/*global JSLINT */
-/*jslint rhino: true, strict: false */
-
 (function (a) {
-    var e, i, input;
+    /*extern JSLINT */
+    importPackage(java.lang);
+
+    var pwd = "util/lint/";
+    load(pwd + "fulljslint.js");
+
     if (!a[0]) {
         print("Usage: jslint.js file.js");
         quit(1);
     }
-    input = readFile(a[0]);
-    if (!input) {
-        print("jslint: Couldn't open file '" + a[0] + "'.");
+    filesArray = a[0].split(";");
+    for (var i = 0; i < filesArray.length; i++) {
+      var currentFile = filesArray[i];
+      print(currentFile);
+      var input = readFile(currentFile);
+      if (!input) {
+        print("jslint: Couldn't open file '" + currentFile + "'.");
         quit(1);
-    }
-    if (!JSLINT(input, {bitwise: true, eqeqeq: true, immed: true,
-            newcap: true, nomen: true, onevar: true, plusplus: true,
-            regexp: true, rhino: true, undef: true, white: true})) {
-        for (i = 0; i < JSLINT.errors.length; i += 1) {
-            e = JSLINT.errors[i];
+      }
+      load(pwd + "jslint.conf");
+      if (!JSLINT(input, JSLINT_OPTIONS)) {
+        for (var i = 0; i < JSLINT.errors.length; i += 1) {
+            var e = JSLINT.errors[i];
             if (e) {
-                print('Lint at line ' + e.line + ' character ' +
-                        e.character + ': ' + e.reason);
+                print('Lint at line ' + (e.line + 1) + ' character ' +
+                        (e.character + 1) + ': ' + e.reason);
                 print((e.evidence || '').
                         replace(/^\s*(\S*(\s+\S+)*)\s*$/, "$1"));
                 print('');
             }
         }
-        quit(2);
-    } else {
+      } else {
         print("jslint: No problems found in " + a[0]);
         quit();
+      }
     }
-}(arguments));
+})(arguments);
+
