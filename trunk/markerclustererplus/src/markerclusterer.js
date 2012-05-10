ac6edfbfff4e01b870ac0184c9c2254bc4c402f3
garylittleRLP <garylittleRLP@b4c4f48c-70f0-11de-9ad3-cbed566d3045>
2012-05-10 14:38:52 +0000

    MarkerClustererPlus 2.0.13: Allow different tooltips for each cluster icon.

--- trunk/markerclustererplus/src/markerclusterer.js
+++ trunk/markerclustererplus/src/markerclusterer.js
@@ -3,7 +3,7 @@
 
 /**
  * @name MarkerClustererPlus for Google Maps V3
- * @version 2.0.12 [April 21, 2012]
+ * @version 2.0.13 [May 8, 2012]
  * @author Gary Little
  * @fileoverview
  * The library creates and manages per-zoom-level clusters for large amounts of markers.
@@ -86,6 +86,9 @@
  * @property {string} text The text of the label to be shown on the cluster icon.
  * @property {number} index The index plus 1 of the element in the <code>styles</code>
  *  array to be used to style the cluster icon.
+ * @property {string} title The tooltip to display when the mouse moves over the cluster icon.
+ *  If this value is <code>undefined</code> or <code>""</code>, <code>title</code> is set to the
+ *  value of the <code>title</code> property passed to the MarkerClusterer.
  */
 /**
  * A cluster icon.
@@ -251,7 +254,11 @@ ClusterIcon.prototype.show = function () {
     } else {
       this.div_.innerHTML = this.sums_.text;
     }
-    this.div_.title = this.cluster_.getMarkerClusterer().getTitle();
+    if (typeof this.sums_.title === "undefined" || this.sums_.title === "") {
+      this.div_.title = this.cluster_.getMarkerClusterer().getTitle();
+    } else {
+      this.div_.title = this.sums_.title;
+    }
     this.div_.style.display = "";
   }
   this.visible_ = true;
@@ -610,7 +617,8 @@ Cluster.prototype.isMarkerAlreadyAdded_ = function (marker) {
  *  set to <code>true</code> if the <code>url</code> fields in the <code>styles</code> array
  *  refer to image sprite files.
  * @property {string} [title=""] The tooltip to display when the mouse moves over a cluster
- *  marker.
+ *  marker. (Alternatively, you can use a custom <code>calculator</code> function to specify a
+ *  different tooltip for each cluster marker.)
  * @property {function} [calculator=MarkerClusterer.CALCULATOR] The function used to determine
  *  the text to be displayed on a cluster marker and the index indicating which style to use
  *  for the cluster marker. The input parameters for the function are (1) the array of markers
@@ -623,7 +631,10 @@ Cluster.prototype.isMarkerAlreadyAdded_ = function (marker) {
  *  <code>index</code> minus 1. For example, the default <code>calculator</code> returns a
  *  <code>text</code> value of <code>"125"</code> and an <code>index</code> of <code>3</code>
  *  for a cluster icon representing 125 markers so the element used in the <code>styles</code>
- *  array is <code>2</code>.
+ *  array is <code>2</code>. A <code>calculator</code> may also return a <code>title</code>
+ *  property that contains the text of the tooltip to be used for the cluster marker. If
+ *   <code>title</code> is not defined, the tooltip is set to the value of the <code>title</code>
+ *   property for the MarkerClusterer.
  * @property {string} [clusterClass="cluster"] The name of the CSS class defining general styles
  *  for the cluster markers. Use this class to define CSS styles that are not set up by the code
  *  that processes the <code>styles</code> array.
@@ -1567,6 +1578,7 @@ MarkerClusterer.prototype.extend = function (obj1, obj2) {
  */
 MarkerClusterer.CALCULATOR = function (markers, numStyles) {
   var index = 0;
+  var title = "";
   var count = markers.length.toString();
 
   var dv = count;
@@ -1578,7 +1590,8 @@ MarkerClusterer.CALCULATOR = function (markers, numStyles) {
   index = Math.min(index, numStyles);
   return {
     text: count,
-    index: index
+    index: index,
+    title: title
   };
 };
 
