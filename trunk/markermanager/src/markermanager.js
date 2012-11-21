8c9bedd37270c4e2c06912f26ba2f7809e8e1f96
cbro@google.com <cbro@google.com@b4c4f48c-70f0-11de-9ad3-cbed566d3045>
2012-11-21 23:12:45 +0000

    Fix "Cannot call method 'substr' of undefined" error

--- trunk/markermanager/src/markermanager.js
+++ trunk/markermanager/src/markermanager.js
@@ -95,7 +95,7 @@ MarkerManager.prototype.initialize = function (map, opt_opts) {
   // Find max zoom level
   var mapMaxZoom = 1;
   for (var sType in mapTypes ) {
-    if (typeof map.mapTypes.get(sType) === 'object' && typeof map.mapTypes.get(sType).maxZoom === 'number') {
+    if (mapTypes.hasOwnProperty(sType) && mapTypes.get(sType).maxZoom === 'number') {
       var mapTypeMaxZoom = map.mapTypes.get(sType).maxZoom;
       if (mapTypeMaxZoom > mapMaxZoom) {
         mapMaxZoom = mapTypeMaxZoom;
