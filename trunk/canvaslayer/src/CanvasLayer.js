29c0beeec56861c268f1710ad22b16cd4fd14de9
brett.mclain@gmail.com <brett.mclain@gmail.com@b4c4f48c-70f0-11de-9ad3-cbed566d3045>
2013-09-12 05:44:28 +0000

    Fixed Issue #260.  Changed a call to setPane that was intended to set the pane by name to use the correct function.

--- trunk/canvaslayer/src/CanvasLayer.js
+++ trunk/canvaslayer/src/CanvasLayer.js
@@ -237,7 +237,7 @@ CanvasLayer.prototype.setOptions = function(options) {
   }
 
   if (options.paneName !== undefined) {
-    this.setPane(options.paneName);
+    this.setPaneName(options.paneName);
   }
 
   if (options.updateHandler !== undefined) {
