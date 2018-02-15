045398d8a8d044bfb8b91082e9de3c01080e497a
Stephen McDonald <steve@jupo.org>
2018-02-16 10:20:38 +1100

    MarkerWithLabel fix for latest maps version. Closes #393.

--- markerwithlabel/src/markerwithlabel.js
+++ markerwithlabel/src/markerwithlabel.js
@@ -306,11 +306,15 @@ MarkerLabel_.prototype.onAdd = function () {
  */
 MarkerLabel_.prototype.onRemove = function () {
   var i;
-  this.labelDiv_.parentNode.removeChild(this.labelDiv_);
+  if (this.labelDiv_.parentNode) {
+    this.labelDiv_.parentNode.removeChild(this.labelDiv_);
+  }
 
   // Remove event listeners:
-  for (i = 0; i < this.listeners_.length; i++) {
-    google.maps.event.removeListener(this.listeners_[i]);
+  if (this.listeners_) {
+    for (i = 0; i < this.listeners_.length; i++) {
+      google.maps.event.removeListener(this.listeners_[i]);
+    }
   }
 };
 
