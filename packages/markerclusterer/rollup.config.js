37c2a570c318122df57b83140f5f54665b9359e5
Justin Poehnelt <jpoehnelt@google.com>
2019-10-04 07:47:59 -0700

    reorg packages and markerclusterer rollup (#515)

---
+++ packages/markerclusterer/rollup.config.js
@@ -0,0 +1,31 @@
+import { terser } from 'rollup-plugin-terser';
+
+export default [
+    {
+        input: 'src/markerclusterer.js',
+        plugins: [terser()],
+        output: {
+            file: 'dist/markerclusterer.umd.js',
+            format: 'umd',
+            name: 'MarkerClusterer',
+            esModule: false
+        }
+    },
+    {
+        input: 'src/markerclusterer.js',
+        plugins: [terser()],
+        output: {
+            file: 'dist/markerclusterer.min.js',
+            format: 'iife',
+            name: 'window',
+            extend: true,
+        }
+    },
+    {
+        input: 'src/markerclusterer.js',
+        output: {
+            file: 'dist/markerclusterer.esm.js',
+            format: 'esm'
+        }
+    }
+];
