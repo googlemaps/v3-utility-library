15f32f21f25dad96d9c3dbd61f91e3e25940fcf4
Timofei Iatsenko <madjestic@gmail.com>
2019-12-17 21:36:37 +0300

    build(markerclustererplus): new rollup config, use default as UMD entry
    rollup will use default export from index.ts as entry for UMD and
    iife bundles.
    
    Simpler rollup config without duplicating input parts.

--- packages/markerclustererplus/rollup.config.js
+++ packages/markerclustererplus/rollup.config.js
@@ -1,63 +1,50 @@
-import { terser } from "rollup-plugin-terser";
-import typescript from "rollup-plugin-typescript2";
-import commonjs from "rollup-plugin-commonjs";
-import babel from "rollup-plugin-babel";
+import {terser} from 'rollup-plugin-terser';
+import typescript from 'rollup-plugin-typescript2';
+import babel from 'rollup-plugin-babel';
 
 const babelOptions = {
-  extensions: [".js", ".ts"],
+  extensions: ['.js', '.ts'],
 
-  presets: ["@babel/env"]
+  presets: ['@babel/env'],
 };
 
-const terserOptions = { output: { comments: "" } };
+const terserOptions = {output: {comments: ''}};
 
 export default [
   {
-    input: "src/index.ts",
+    input: 'src/index.ts',
     plugins: [
       typescript(),
-      commonjs(),
       babel(babelOptions),
       terser(terserOptions)
     ],
-    output: {
-      file: "dist/markerclustererplus.umd.js",
-      format: "umd",
-      sourcemap: true,
-      name: "MarkerClusterer"
-    }
+    output: [
+      {
+        file: 'dist/markerclustererplus.umd.js',
+        format: 'umd',
+        sourcemap: true,
+        exports: 'default',
+        name: 'MarkerClusterer',
+      },
+      {
+        file: 'dist/markerclustererplus.min.js',
+        format: 'iife',
+        sourcemap: true,
+        exports: 'default',
+        name: 'MarkerClusterer',
+      },
+    ],
   },
   {
-    input: "src/index.ts",
+    input: 'src/index.ts',
     plugins: [
       typescript(),
-      commonjs(),
-      babel(babelOptions),
-      terser(terserOptions)
     ],
     output: {
-      file: "dist/markerclustererplus.min.js",
-      format: "iife",
-      name: "MarkerClusterer"
-    }
-  },
-  {
-    input: "src/index.ts",
-    plugins: [typescript(), commonjs(), babel(babelOptions)],
-    output: {
-      file: "dist/markerclustererplus.dev.js",
-      format: "iife",
-      name: "MarkerClusterer",
-    }
-  },
-  {
-    input: "src/index.ts",
-    plugins: [typescript()],
-    output: {
-      file: "dist/markerclustererplus.esm.js",
-      format: "esm",
+      file: 'dist/markerclustererplus.esm.js',
+      format: 'esm',
       sourcemap: true,
-      name: "MarkerClusterer",
-    }
-  }
+      name: 'MarkerClusterer',
+    },
+  },
 ];
